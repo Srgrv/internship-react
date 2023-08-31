0a09afa114ecaa92029926a0baa5649478ed96a0
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-31 14:19:05 +0300

    create footer and css for it

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//style
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      &copy; 2023 Maxima School. Все права защищены.
+    </footer>
+  );
+};
+
+export default Footer;
